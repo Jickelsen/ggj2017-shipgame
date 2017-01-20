50e44099381dd6852dac55ce0b34f4cb9d95931a
Jacob Michelsen <chromegadget@gmail.com>
2017-01-20 21:52:15 +0100

    Added standard controller assets, a wave manager and some general managers

---
+++ Assets/Scripts/Managers/GeneralManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralManager : MonoBehaviour {
+
+    public WaveManager WaveManager;
+    public Vector4[] WavesAmpxPerxAmpzPerz;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnLevelLoaded(int pLevel) {
+        Debug.Log("Level loaded" + pLevel);
+        WaveManager.AmplitudeX = WavesAmpxPerxAmpzPerz[pLevel].x;
+        WaveManager.PeriodX = WavesAmpxPerxAmpzPerz[pLevel].y;
+        WaveManager.AmplitudeZ = WavesAmpxPerxAmpzPerz[pLevel].z;
+        WaveManager.PeriodZ = WavesAmpxPerxAmpzPerz[pLevel].w;
+    }
+}
