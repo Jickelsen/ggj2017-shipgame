1bbd546072323a2919e8e008e06c72f122fcdd4a
Jacob Michelsen <chromegadget@gmail.com>
2017-01-21 11:37:52 +0100

    Slope movement and adjusted scale

---
+++ Assets/Scripts/Movement/SlopeMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlopeMovement : MonoBehaviour {
+
+    public float Slipperyness;
+    CharacterController _characterController;
+
+	// Use this for initialization
+	void Start () {
+        _characterController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
+            Vector3 horizontalVector = new Vector3(Slipperyness * hit.normal.x, 0f, Slipperyness * hit.normal.z);
+            Vector3 projectedVector = Vector3.Project(horizontalVector, hit.normal);
+            Vector3 groundVector = horizontalVector - projectedVector;
+            _characterController.Move(Slipperyness * groundVector);
+        }
+    }
+
+}
