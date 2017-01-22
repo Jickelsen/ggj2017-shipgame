e04be8535ea37ae365ba6e5c814629742f5652f6
Jacob Michelsen <chromegadget@gmail.com>
2017-01-22 14:17:59 +0100

    Knife placeholder prefab with sound effects

---
+++ Assets/Scripts/Environment/PlaySoundOnAcc.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnAcc : MonoBehaviour {
+
+    public float AccelerationToMakeSound;
+    public AudioClip[] Clips;
+    AudioSource _audioSource;
+    Vector3 _velocity;
+    Vector3 _acceleration;
+    Vector3 _lastPos;
+    Vector3 _lastVelocity;
+    bool _playing = false;
+
+    private void Awake()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+    }
+    // Use this for initialization
+    void Start () {
+        _lastPos = transform.position;
+
+	}
+
+    void LateUpdate() {
+        Vector3 lastAcceleration = _acceleration;
+        _velocity = (transform.position - _lastPos) / Time.deltaTime;
+        if (_lastVelocity == null) {
+            _lastVelocity = _velocity;
+        }
+        _acceleration = (_velocity - _lastVelocity) / Time.deltaTime;
+        //Debug.Log("Acceleration " + _acceleration.magnitude);
+        //_acceleration = Vector3.Lerp(lastAcceleration, _acceleration, Time.deltaTime);
+        if (_acceleration.magnitude >= AccelerationToMakeSound && !_playing) {
+            StartCoroutine(PlaySound());
+        }
+        _lastPos = transform.position;
+        _lastVelocity = _velocity;
+    }
+
+    IEnumerator PlaySound() {
+        _playing = true;
+        _audioSource.PlayOneShot(Clips[(int)Mathf.Floor(Random.value*3.99f)]);
+        Debug.Log("Playing clip " + (int)Mathf.Floor(Random.value * 3.99f));
+        yield return new WaitForSeconds(2f);
+        _playing = false;
+    }
+
+}
